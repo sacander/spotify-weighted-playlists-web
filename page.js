09a170e5be24864135c81bc100781f49a9872157
Sacander <89387718+Sacander@users.noreply.github.com>
2021-09-16 00:09:30 +0930

    session storage

--- page.js
+++ page.js
@@ -1,13 +1,21 @@
 function countdownTimer(){
     if (window.location.hash.length != 0) {
-        let accessTokenExpiresIn = 3600;
-        let minutes = String(Math.floor(accessTokenExpiresIn/60)).padStart(2, "0")
-        let seconds = String(accessTokenExpiresIn%60).padStart(2, "0")
+
+        let accessTokenExpiresIn = 0;
+        let minutes = "";
+        let seconds = "";
+
+        if (sessionStorage.getItem("accessTokenExpiresIn") == null) {
+            accessTokenExpiresIn = 3600;
+        } else {
+            accessTokenExpiresIn = sessionStorage.getItem("accessTokenExpiresIn");
+        }
 
         setInterval(function() {
             accessTokenExpiresIn -= 1;
-            minutes = String(Math.floor(accessTokenExpiresIn/60)).padStart(2, "0")
-            seconds = String(accessTokenExpiresIn%60).padStart(2, "0")
+            sessionStorage.setItem("accessTokenExpiresIn", accessTokenExpiresIn);
+            minutes = String(Math.floor(accessTokenExpiresIn/60)).padStart(2, "0");
+            seconds = String(accessTokenExpiresIn%60).padStart(2, "0");
             document.getElementById("accessTokenExpiresIn").innerHTML = "Access token expires in " + minutes + ":" + seconds;
         }, 1000);
     }
