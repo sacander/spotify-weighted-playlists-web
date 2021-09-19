ff84a563b62783be17b1e882b3bcf0a5b1fd823e
Sacander <89387718+Sacander@users.noreply.github.com>
2021-09-19 21:25:53 +0930

    weightedTrackArray()

--- app.js
+++ app.js
@@ -38,9 +38,10 @@ async function app(){
         
         const accessToken = getAccessToken(window.location);
         let tracks = await getPlaylistItems(accessToken, document.getElementById("inputPlaylistId").innerHTML);
+        let x = await getPlaylistItems(accessToken, "29eVFm6KNSJnfUcRghmWab")
         let tracksByTaylorSwift = tracks.filter(filterByArtist("Taylor Swift"));
-        // replacePlaylist(accessToken, document.getElementById("outputPlaylistId").innerHTML, tracks);
-        console.log(cumulativeProbabilities([2, 1, 3]));
+        y = weightedTrackArray([tracks, x], [5, 1])
+        replacePlaylist(accessToken, document.getElementById("outputPlaylistId").innerHTML, y)
 
     }
 
@@ -151,7 +152,7 @@ function filterByArtist(artist) {
 }
 
 // Takes an array of numbers, converts into relative probabilities, then makes it cumulative
-function cumulativeProbabilities(probabilityValues) { // Array of numbers
+function getCumulativeProbabilities(probabilityValues) { // Array of numbers
     const cumulativeProbabilities = [0];
 
     let sum = 0;
@@ -165,4 +166,38 @@ function cumulativeProbabilities(probabilityValues) { // Array of numbers
 
     cumulativeProbabilities.shift(); // Removes initial 0 in cumulativeProbabilities
     return cumulativeProbabilities; // Returns array of numbers
+}
+
+// Takes an array of custom track object arrays, and an array of corresponding probability values
+function weightedTrackArray(trackArrays, probabilityValues) {
+    const weightedTrackArray = [];
+    const cumulativeProbabilities = getCumulativeProbabilities(probabilityValues);
+    let random = 0;
+    let randomIndex = 0;
+
+    trackArrays.forEach((trackArray, index) => { // Adds a marker for when an array has been looped, the corresponding probability and a backup to restore from
+        trackArray.unlooped = true;
+        trackArray.probability = cumulativeProbabilities[index];
+        trackArray.backup = Array.from(trackArray);
+    });
+
+    while (trackArrays.some(trackArray => trackArray.unlooped)) { // While at least one track array has not been looped through
+        random = Math.random();
+
+        for (let trackArray of trackArrays) { // Iterate through each track array
+            if (random < trackArray.probability) { // If the random number is less than probability threshold
+                randomIndex = Math.floor(Math.random() * trackArray.length); // Get random number from 0 to length - 1
+                weightedTrackArray.push(...trackArray.splice(randomIndex, 1)); // Remove track object from track array and add it to outputt array
+
+                if (trackArray.length == 0) { // If track array is empty, make it as looped and reset it
+                    trackArray.unlooped = false;
+                    trackArray.push(...trackArray.backup);
+                }
+                break;
+            }
+        }
+
+    }
+
+    return weightedTrackArray; // Returns array of custom track objects
 }
