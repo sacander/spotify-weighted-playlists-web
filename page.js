f45b37454d2011baac5de24df2b5eabd2350220c
Zach <zanderson004@gmail.com>
2024-01-09 20:05:39 +1030

    change redirect

--- page.js
+++ page.js
@@ -1,7 +1,7 @@
 // #region Global Constants
 const clientId="56c1bd9731fc4880988e268fe1e85eec"; // Spotify app client id
-// const redirectUrl="https://zanderson004.github.io/spotify-weighted-playlists-web/"; // Spotify app redirect url
-const redirectUrl="http://localhost:8000/"
+const redirectUrl="https://zanderson004.github.io/spotify-weighted-playlists-web/"; // Spotify app redirect url
+// const redirectUrl="http://localhost:8000/"
 const scope = "playlist-modify-public playlist-modify-private"; // Scopes to access spotify api
 //#endregion
 
